49dd7ce1aa36460f89fdc868102d209cdb1b3733
Julio María Meca Hansen <jmecahansen@gmail.com>
2021-07-03 13:54:11 +0200

    Added SQL definitions for the application tables

---
+++ db/tables.sql
@@ -0,0 +1,3 @@
+CREATE TABLE IF NOT EXISTS t_users (user_id integer PRIMARY KEY, user_name_first text NOT NULL, user_name_last text NOT NULL, user_phone text NOT NULL, user_dni text NOT NULL);
+CREATE TABLE IF NOT EXISTS t_users_location (location_id integer PRIMARY KEY, user_id integer NOT NULL, location_address text NOT NULL, location_city text NOT NULL, location_country text NOT NULL, location_zip text NOT NULL, FOREIGN KEY (user_id) REFERENCES t_users (user_id));
+CREATE TABLE IF NOT EXISTS t_users_records (record_id integer PRIMARY KEY, user_id integer NOT NULL, record_title text NOT NULL, record_year text NOT NULL, FOREIGN KEY (user_id) REFERENCES t_users (user_id));
